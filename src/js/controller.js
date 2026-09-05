524d07fe4a1211a74cadabb0537c388b89ca87fb
Sahel <sahelstha2061@gmail.com>
2026-09-05 18:12:30 +0545

    feat(search): make the hashurl when clicking the recipie

--- src/js/controller.js
+++ src/js/controller.js
@@ -27,10 +27,14 @@ const renderSpinner = parentEl => {
 
 const showRecipe = async () => {
   try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886',
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`,
     );
 
     const data = await res.json();
@@ -147,4 +151,4 @@ const showRecipe = async () => {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
