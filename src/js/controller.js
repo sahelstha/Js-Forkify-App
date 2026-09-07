81a705de7255ffdc659ff06e276b0fb1b1193895
Sahel <sahelstha2061@gmail.com>
2026-09-07 09:18:07 +0545

    feat(bookmark): add the bookmark functionality to the recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -60,9 +60,19 @@ const controlServings = newServing => {
   recipeView.update(model.state.recipe);
 };
 
+const controlAddBookmark = () => {
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  else model.deleteBookmark(model.state.recipe.id);
+
+  console.log(model.state.recipe);
+
+  recipeView.update(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipe);
   recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
